45f02930234c8c4144d7eccf6df06bb3778df335
Sheikh Rashed <mdrashed1310@gmail.com>
2021-04-01 12:05:18 +0600

    express-ejs-layouts implementing ...

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // @ts-nocheck
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./routes/index'));
